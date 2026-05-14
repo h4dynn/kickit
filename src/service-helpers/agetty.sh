aae6d65b5af749972423c3f563185c511f223639
h4dynn <h4dynn@github.com>
2026-05-14 18:43:08 +0100

    init(script): improve agetty helper, include in lint workflow

--- src/service-helpers/agetty.sh
+++ src/service-helpers/agetty.sh
@@ -9,14 +9,14 @@
 # ############################################### #
 
 # busybox
-if command -v getty &> /dev/null
+if command -v getty > /dev/null
 then {
-  export GETTY=getty
+  readonly GETTY=getty
 }
 # util-linux
-elif command -v agetty &> /dev/null
+elif command -v agetty > /dev/null
 then {
-  export GETTY=agetty
+  readonly GETTY=agetty
 }
 else {
   echo "Failed to find a suitable agetty client!" >&2
@@ -26,21 +26,24 @@ fi
 
 if [ -r /etc/agetty.conf ]
 then {
+  # shellcheck source=/dev/null
   . /etc/agetty.conf
 }
 else {
   # The default configuration options
-  export TTYS=7
-  export BAUD_RATE=38400
-  export TERM_NAME=linux
+  readonly TTYS=7
+  readonly BAUD_RATE=38400
+  readonly TERM_NAME=linux
 }
 fi
 
-for ((tty = 0; $tty < $TTYS; tty++))
+tty=0
+while [ $tty -le "$TTYS" ]
 do {
+  tty=$((tty+1))
   echo "Opening tty ${tty}/${TTYS}" >&2
-  exec "${GETTY}" "${GETTY_ARGS[@]}" \
-                 "${tty}" "${BAUD_RATE}" "${TERM_NAME}" &
+  # Open agetty in the background
+  exec "${GETTY}" "${GETTY_ARGS}" "${tty}" "${BAUD_RATE}" "${TERM_NAME}" &
 }
 done
 
